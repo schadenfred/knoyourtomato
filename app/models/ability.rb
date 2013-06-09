e65bff55a48d6f0bda1d9fa154d646ee163fcaa0
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-06-08 20:30:52 -0700

    finished admin page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.has_role? :admin
+    if user.has_role? :superadmin
       can :manage, :all
     end
   end
