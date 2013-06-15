ee6d4e16aadd5ce3254a4ea09f6aea087286a4fb
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-06-14 21:26:57 -0700

    spaced top of body container to accommodate logo image.  organized links.  added favicon

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,5 @@
 class Community < ActiveRecord::Base
   attr_accessible :cuisine, :description, :languages, :name, :nearest_town, :population, :practices, :religions
+
+  has_many :farms
 end
--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,3 +1,5 @@
 class Farm < ActiveRecord::Base
   attr_accessible :area, :fruit, :community_id, :description, :name, :planted_on, :soil_type
+
+  belongs_to :community 
 end
