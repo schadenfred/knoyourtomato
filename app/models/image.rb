69cb3fe345cf7ff026cc8f50620cf5e95826950e
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-06-16 23:31:21 -0700

    centered logo image, did image in canonical way, removed fixed navbar styles from nav_bar_top

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
-  attr_accessible :name, :imageable_id, :imageable_type
+  attr_accessible :image, :name, :imageable_id, :imageable_type
 
   belongs_to :imageable, polymorphic: true
 
   mount_uploader :image, ImageUploader
-  
+
 end
